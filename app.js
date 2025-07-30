064361023c3d3065d77adf4bd8c0a902b07963fa
KrishnaKC15 <nalinichauhan99@gmail.com>
2025-07-30 14:52:09 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,135 @@
+//1 is for red
+//2 is for teal
+//3 is for orange
+//4 is for blue
+
+
+let sequence=[];
+let score=0;
+let check=false;
+let count=0;
+let lock =true;
+let highScore=0;
+let body=document.querySelector('body');
+body.addEventListener("keydown",function(){
+    if(check==false)
+    {
+        check=true;
+        level.innerText=`Level ${score+1}`;
+        setTimeout(() => {
+            startgame();
+        }, 2000);
+        
+    }
+})
+
+let redBox=document.querySelector('.red');
+let tealBox=document.querySelector('.teal');
+let orangeBox=document.querySelector('.orange');
+let blueBox=document.querySelector('.blue');
+let ans;
+let level=document.querySelector('.levels');
+let high=document.querySelector('.high');
+let info=document.querySelector('.information')
+
+function startgame(){
+
+    
+    let randomNo=Math.ceil(Math.random()*4);
+    console.log(randomNo)
+    sequence.push(randomNo);
+    
+    if(randomNo==1)
+        flash(redBox)
+    if(randomNo==2)
+        flash(tealBox)
+    if(randomNo==3)
+        flash(orangeBox)
+    if(randomNo==4)
+        flash(blueBox)
+    lock=false
+}
+
+function flash(boxName){
+    boxName.classList.add('select');
+    setTimeout(function(){
+    boxName.classList.remove('select');
+    },150)
+
+    
+}
+
+redBox.addEventListener('click',function(){
+    if(check==true &&lock==false)
+    {
+        ans=1;
+        flash(redBox)
+        checkans();
+    }
+})
+tealBox.addEventListener('click',function(){
+    if(check==true &&lock==false)
+    {
+        ans=2;
+        flash(tealBox)
+        checkans();
+    }
+}
+)
+orangeBox.addEventListener('click',function(){
+    if(check==true &&lock==false)
+    {
+        ans=3;
+        flash(orangeBox)
+        checkans();
+    }
+})
+blueBox.addEventListener('click',function(){
+    if(check==true &&lock==false)
+    {
+        ans=4;
+        flash(blueBox)
+        checkans();
+    }
+}
+)
+
+
+function checkans(){
+    if(ans==sequence[count]&&count==sequence.length-1)
+    {
+        count=0;
+        score++;
+        console.log(score);
+        lock=true;
+        level.innerText=`Level ${score+1}`;
+        setTimeout(() => {
+            startgame();
+        }, 2000);
+    }
+    else if(ans==sequence[count])
+    {
+        count++;
+    }
+    else if(ans!=sequence[count])
+    {
+        if(highScore<score)
+        {
+            highScore=score;
+            high.innerText=`High Score: ${highScore}`;
+            
+        }
+            
+        ans=0;
+        count=0;
+        sequence.splice(0,sequence.length);
+        body.style.backgroundColor='red';
+        lock=true;
+        setTimeout(()=>{
+            body.style.backgroundColor='white';
+        },150);
+        level.innerHTML=`<span>Wrong Sequence.</span><br>Your Score was <strong>${score}</strong>.<br>Press Any key to restart`
+        check=false;
+        score=0;
+    }
+}
